dc0bd84d87c4c499d7f8bd75ed607480bfcd1d9b
Nikhil <nikhilcogni@gmail.com>
2025-05-13 21:36:43 +0530

    ButtonHandling Tests

---
+++ tests/ButtonHandling.spec.ts
@@ -0,0 +1,25 @@
+import {expect, test} from '@playwright/test';
+
+test('Validating the left click on the button', async({page})=>
+{
+    await page.goto("https://play1.automationcamp.ir/mouse_events.html");
+    await page.locator("#click_area").click();
+    await expect(page.locator("#click_type")).toHaveText("Click");
+    await page.close();
+});
+
+test('Validating the Double click on the button', async({page})=>
+    {
+        await page.goto("https://play1.automationcamp.ir/mouse_events.html");
+        await page.locator("#click_area").dblclick();
+        await expect(page.locator("#click_type")).toHaveText("Double-Click");
+        await page.close();
+    });
+
+test('Validating the right click on the button', async({page})=>
+{
+        await page.goto("https://play1.automationcamp.ir/mouse_events.html");
+        await page.locator("#click_area").click({button:"right"});
+        await expect(page.locator("#click_type")).toHaveText("Right-Click");
+        await page.close();
+});
